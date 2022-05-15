0253c84b41538375c6f54568b3394cb697aa4fdb
Sanjar <aslonovsanjar02@gmail.com>
2022-05-15 05:50:31 +0500

    Initial Commit

---
+++ StoryboardContact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardContact
+//
+//  Created by Sanjar Aslonov on 15/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
